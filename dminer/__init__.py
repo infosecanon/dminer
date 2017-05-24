451e58daf2d910be43164c642cda34907526d430
Bobby <btonic@hackucf.org>
2017-05-24 10:13:27 -0400

    Add description for the module

--- dminer/__init__.py
+++ dminer/__init__.py
@@ -1,5 +1,6 @@
 """
-TODO: DOC
+The dminer module provides both CLI wrapping interfaces, as well as direct
+library access to all configuration, ingestion, and scraping interfaces.
 """
 import ingestion
 import sinks
